87dc29b1f4231c84c879bb2f68d3fa7720f077da
Jesse Duffield <jessedduffield@gmail.com>
2023-06-03 13:15:41 +1000

    Only cancel search if main or temporary context loses focus
    This is a pickle: initially I wanted it so that a filter would cancel automatically if the current context lost focus.
    But there are situations where you want to retain the focus, e.g. when a popup appears, or when you view the commits
    of a branch. The issue is that when you view the commits of a branch, the branches context is removed from the context
    stack. Even if this were not the case, you could imagine going branches -> sub-commits -> files -> sub-commits, where
    in that case branches would definitely be off the stack upon navigating to the files context.
    
    So because I'm too lazy to find a proper solution to this problem, I'm just making it so that filters in side contexts
    are retained unless explicitly cancelled.
    
    There's another edge case this commit handles which is that if I'm in the sub-commits context via the branches context
    and start a search, then navigate to the reflog context and hit enter to get to the sub-commits context again, I need
    to cancel the search before I switch. Likewise with the commit files context.

--- pkg/gui/controllers/helpers/search_helper.go
+++ pkg/gui/controllers/helpers/search_helper.go
@@ -35,15 +35,16 @@ func (self *SearchHelper) OpenFilterPrompt(context types.IFilterableContext) err
 	return nil
 }
 
-func (self *SearchHelper) OpenSearchPrompt(context types.Context) error {
+func (self *SearchHelper) OpenSearchPrompt(context types.ISearchableContext) error {
 	state := self.searchState()
 
 	state.Context = context
+	searchString := context.GetSearchString()
 
 	self.searchPrefixView().SetContent(self.c.Tr.SearchPrefix)
 	promptView := self.promptView()
-	// TODO: should we show the currently searched thing here? Perhaps we can store that on the context
 	promptView.ClearTextArea()
+	promptView.TextArea.TypeString(searchString)
 	promptView.RenderTextArea()
 
 	if err := self.c.PushContext(self.c.Contexts().Search); err != nil {
@@ -72,7 +73,8 @@ func (self *SearchHelper) DisplaySearchPrompt(context types.ISearchableContext) 
 	state.Context = context
 	searchString := context.GetSearchString()
 
-	self.searchPrefixView().SetContent(self.c.Tr.SearchPrefix)
+	_ = context.GetView().SelectCurrentSearchResult()
+
 	promptView := self.promptView()
 	promptView.ClearTextArea()
 	promptView.TextArea.TypeString(searchString)
@@ -173,7 +175,7 @@ func (self *SearchHelper) Cancel() {
 		// do nothing
 	}
 
-	state.Context = nil
+	self.HidePrompt()
 }
 
 func (self *SearchHelper) OnPromptContentChanged(searchString string) {
@@ -222,4 +224,9 @@ func (self *SearchHelper) CancelSearchIfSearching(c types.Context) {
 		}
 		return
 	}
+}
+
+func (self *SearchHelper) HidePrompt() {
+	state := self.searchState()
+	state.Context = nil
 }
